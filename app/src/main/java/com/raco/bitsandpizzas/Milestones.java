e1a9c401c4bf1ee5eb177769a16b4518e767727b
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-10 14:04:16 -0400

    Test adding a toolbar to the app.

--- app/src/main/java/com/raco/bitsandpizzas/Milestones.java
+++ app/src/main/java/com/raco/bitsandpizzas/Milestones.java
@@ -1,6 +1,7 @@
 package com.raco.bitsandpizzas;
 
 public class Milestones {
-    // Test adding the AppBar
+    // Test adding the AppBar.
     // Test removing the AppBar in order to use a Toolbar.
+    // Test adding a toolbar to the app.
 }
