25c035b2d1a644353f5990e607cbdaea345f6c0f
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-10 15:21:23 -0400

    Test adding a share action provider to the toolbar.

--- app/src/main/java/com/raco/bitsandpizzas/Milestones.java
+++ app/src/main/java/com/raco/bitsandpizzas/Milestones.java
@@ -3,6 +3,7 @@ package com.raco.bitsandpizzas;
 public class Milestones {
     // Test adding the AppBar.
     // Test removing the AppBar in order to use a Toolbar.
-    // Test adding a toolbar to the app.
+    // Test adding a toolbar to the application.
     // Test adding an action to the toolbar
+    // Test adding a share action provider to the toolbar.
 }
