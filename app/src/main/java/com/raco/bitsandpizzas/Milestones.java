5d8e07b3734360c77779504e756b3784abc471e8
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-10 14:45:10 -0400

    Test adding an action to the toolbar

--- app/src/main/java/com/raco/bitsandpizzas/Milestones.java
+++ app/src/main/java/com/raco/bitsandpizzas/Milestones.java
@@ -4,4 +4,5 @@ public class Milestones {
     // Test adding the AppBar.
     // Test removing the AppBar in order to use a Toolbar.
     // Test adding a toolbar to the app.
+    // Test adding an action to the toolbar
 }
